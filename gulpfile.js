0cce09c6612b620cae9ea316c88f5b52062eaa0c
zy <294395576@qq.com>
2018-06-19 10:20:37 +0800

    完成

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var mincss = require('gulp-clean-css'); // 压缩css文件
 var sass = require('gulp-sass'); // 编译scss
 var sequence = require('gulp-sequence'); // 设置任务的执行顺序
 var autoprefixer = require('gulp-autoprefixer'); // 自动添加前缀
+var clean = require('gulp-clean'); // 删除指定文件
+var minhtml = require('gulp-clean-html'); // 压缩html
 var path = require('path'); // 引入node内置模块
 var fs = require('fs'); // 引入node内置模块
 var url = require('url'); // 引入node内置模块
@@ -50,4 +52,52 @@ gulp.task('watch',function(){
 // 开发环境 --- 设置任务执行的顺序
 gulp.task('dev',function(cb){
     sequence('devcss','watch','devServer',cb)
+})
+
+// 线上环境 --- 起服务
+gulp.task('buildServer',function(){
+    return gulp.src('build')
+        .pipe(server({
+            port:9090, // 配置端口号
+            open:true, // 自动打开浏览器
+            middleware:function(req,res,next){ // 拦截请求数据
+                var pathname = url.parse(req.url).pathname;
+                if(pathname === '/favicon.ico'){
+                    return false;
+                }
+                if(pathname == '/api/list'){
+                    res.end(JSON.stringify(mock));
+                }else{
+                    pathname = pathname === '/' ? 'index.html' : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname,'build',pathname)))
+                }
+                
+            }
+        }))
+})
+
+// 删除指定文件
+gulp.task('clean',function(){
+    return gulp.src('build')
+        .pipe(clean());
+})
+
+// 线上环境 --- copycss
+gulp.task('buildCss',function(){
+    return gulp.src('src/css/*css',{base:'src'})
+        .pipe(mincss())
+        .pipe(gulp.dest('build'))
+})
+
+
+// 线上环境 --- copyjs
+gulp.task('buildjs',function(){
+    return gulp.src('src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'))
+})
+
+// 线上环境 --- 设置任务执行的顺序
+gulp.task('build',function(cb){
+    sequence('clean','buildCss','buildjs','buildServer',cb)
 })
